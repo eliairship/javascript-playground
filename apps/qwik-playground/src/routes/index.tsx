88b1b54c8ffa8e7884a3a99efca943d66b45fe1e
Matt Koehler <matt.koehler@unosquare.com>
2024-03-12 08:44:34 -0500

    adding validation

--- apps/qwik-playground/src/routes/index.tsx
+++ apps/qwik-playground/src/routes/index.tsx
@@ -1,7 +1,22 @@
 import { component$ } from "@builder.io/qwik";
+import { routeAction$, zod$, z, Form } from "@builder.io/qwik-city";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+export const useLoginAction = routeAction$(
+  async () => {
+    return { success: true };
+  },
+  // Zod schema is used to validate the FormData
+  zod$({
+    email: z.string().email(),
+    password: z.string().min(1, "Please enter your password"),
+  }),
+);
+
 export default component$(() => {
+  const action = useLoginAction();
+  console.log("🚀 ~ action:", action);
+
   return (
     <div class="flex h-screen flex-col items-center justify-center">
       <div
@@ -16,7 +31,7 @@ export default component$(() => {
             Enter your email below to login to your account.
           </p>
         </div>
-        <form class="space-y-4 p-6">
+        <Form action={action} class="space-y-4 p-6">
           <div class="space-y-2">
             <label
               class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -27,9 +42,10 @@ export default component$(() => {
             <input
               class="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               id="email"
+              name="email"
               placeholder="Email"
-              type="email"
             />
+            {action.value?.failed && <p class='text-red-700'>{action.value.fieldErrors.email}</p>}
           </div>
           <div class="space-y-2">
             <label
@@ -41,19 +57,21 @@ export default component$(() => {
             <input
               class="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
               id="password"
+              name="password"
               placeholder="Password"
               type="password"
             />
+            {action.value?.failed && <p class='text-red-700'>{action.value.fieldErrors.password}</p>}
           </div>
-        </form>
-        <div class="flex items-center p-6">
-          <button
-            class="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-full items-center justify-center whitespace-nowrap rounded-md bg-black px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-            type="submit"
-          >
-            Login
-          </button>
-        </div>
+          <div class="flex items-center p-6">
+            <button
+              class="ring-offset-background focus-visible:ring-ring inline-flex h-10 w-full items-center justify-center whitespace-nowrap rounded-md bg-black px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-black/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
+        </Form>
       </div>
     </div>
   );
