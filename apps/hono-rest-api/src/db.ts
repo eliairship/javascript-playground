447974b70fc2b42fea6d012cc276d735d76b7a1e
Eli Marshall <eli.marshall@unosquare.com>
2024-03-13 10:22:27 -0400

    Add Dockerfile

--- apps/hono-rest-api/src/db.ts
+++ apps/hono-rest-api/src/db.ts
@@ -4,7 +4,6 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import postgres from 'postgres';
 
-console.log(process.env.DB_URL);
 
 export const client = postgres(process.env.DB_URL ?? '');
 
--- apps/hono-rest-api/src/schema.ts
+++ apps/hono-rest-api/src/schema.ts
@@ -1,24 +1,4 @@
-// import { Client } from 'pg';
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-
-// import { env } from 'hono/adapter'
-
-// console.log(env('DB_URL'));
-// console.log(process.env.DB_URL);
-
-// const pool = new pg.Pool({
-//   connectionString: process.env.DB_URL,
-// });
-// export const db = drizzle(pool);
-// const client = postgres(process.env.DB_URL ?? '');
-
-// const client = new Client({
-//   connectionString: process.env.DB_URL,
-//   // connectionString: 'postgres://user:password@host:port/db',
-// });
-// export const db = drizzle(client);
 
 const userTable = pgTable('user', {
   id: text('id').primaryKey(),
